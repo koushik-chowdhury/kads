3dde441a6ff9db451e802f4c548dc7f1fec2d051
Koushik Chowdhury <csakoushik@gmail.com>
2024-11-22 20:49:35 +0530

    add filter

--- job/script.js
+++ job/script.js
@@ -18,3 +18,38 @@ function formatDate() {
   }
 }
 window.addEventListener("load", formatDate);
+
+// FILTER
+
+const buttons = document.getElementById("button");
+const jobs = document.querySelectorAll(".job-post");
+const notice = document.querySelector(".notice");
+function filterJobs(sector) {
+  let visibleJobs = 0;
+  jobs.forEach((job) => {
+    if (sector === "all" || job.getAttribute("data-sector") === sector) {
+      job.style.display = "block"; // Show job
+      visibleJobs++;
+    } else {
+      job.style.display = "none"; // Hide job
+    }
+  });
+  if (visibleJobs === 0) {
+    console.log("hellow world");
+    notice.innerHTML = `<h1> No Jobs available for ${sector} </h1>`;
+    notice.style.display = "block";
+  } else {
+    notice.style.display = "none";
+  }
+}
+
+// Event delegation: listen for click events on the button container
+buttons.addEventListener("click", (e) => {
+  const sector = e.target.getAttribute("data-sector"); // Get the sector from the clicked button's data attribute
+  if (sector) {
+    filterJobs(sector);
+  }
+});
+
+// Optionally, show all jobs by default
+filterJobs("all");
